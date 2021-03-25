99ecf2cd9bd332e7205e802e9ce3781f0d5dbd3f
David McKenzie <34796579+drmckenzie@users.noreply.github.com>
2021-03-25 13:09:34 +0000

    initial commit

---
+++ js/copyToClipboard.js
@@ -0,0 +1,15 @@
+function copyToClipboard(inputName) {
+  /* Get the text field */
+  /*var copyText = document.getElementById("myInput");*/
+  var copyText = document.getElementById(inputName);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
