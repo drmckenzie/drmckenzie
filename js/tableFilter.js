cd182dbe9090018db5dac3a9e9bfa0558e56a3a5
David McKenzie <34796579+drmckenzie@users.noreply.github.com>
2021-04-26 17:59:19 +0100

    Fix?

--- js/tableFilter.js
+++ js/tableFilter.js
@@ -7,7 +7,7 @@ function myFunction() {
   for (var i = 1; i < tr.length; i++) {
     var tds = tr[i].getElementsByTagName("td");
     var flag = false;
-    for(var j = 1; j < tds.length; j++){
+    for(var j = 0; j < tds.length; j++){
       var td = tds[j];
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         flag = true;
