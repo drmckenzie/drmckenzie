3d582429b9cf7f901f78054d4a82869ba81a9d4e
David McKenzie <34796579+drmckenzie@users.noreply.github.com>
2021-04-26 17:58:09 +0100

    Header stop hiding

--- js/tableFilter.js
+++ js/tableFilter.js
@@ -4,10 +4,10 @@ function myFunction() {
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
-  for (var i = 0; i < tr.length; i++) {
+  for (var i = 1; i < tr.length; i++) {
     var tds = tr[i].getElementsByTagName("td");
     var flag = false;
-    for(var j = 0; j < tds.length; j++){
+    for(var j = 1; j < tds.length; j++){
       var td = tds[j];
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         flag = true;
